29259772fef28982065191de2cd443b7f317c26a
Candy999 <leo033071185@gmail.com>
2019-01-12 20:32:50 +0800

    改手手

--- img/run3.0/_pngzip.cmd
+++
@@ -1,7 +0,0 @@
-cd /d %~dp0 
-echo %~d1%~p1%~n1%~x1
-goto %~x1
-:.png
-zopflipng.exe -y -m --iterations=1 --splitting=1 --filters=0 --lossy_8bit "%~d1%~p1%~n1.png" "%~d1%~p1%~n1.png"
-DeflOpt.exe %~d1%~p1%~n1.png
-exit
